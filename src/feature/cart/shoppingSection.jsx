010bf5404c4862480db20dfe00d463e7724cd9df
YinelaPrimero <yinelaprimero01@gmail.com>
2024-05-22 19:10:44 -0500

    Ajustes

--- src/feature/cart/shoppingSection.jsx
+++ src/feature/cart/shoppingSection.jsx
@@ -3,9 +3,9 @@ import { useDispatch } from "react-redux";
 import { addToCart } from "./cartSlice";
 
 const products = [
-  { id: 1, name: "Product A", price: 10 },
-  { id: 2, name: "Product B", price: 20 },
-  { id: 3, name: "Product C", price: 15 },
+  { id: 1, name: "Producto A", price: 1000 },
+  { id: 2, name: "Producto B", price: 2000 },
+  { id: 3, name: "Producto C", price: 1500 },
   // add more products as needed
 ];
 
